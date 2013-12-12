e3a593b1104a757a099f401c8c954cf7dfb3162a
Wush Wu <wush@bridgewell.com>
2013-12-12 14:25:55 +0800

    add cmd prefix

--- R/utility.R
+++ R/utility.R
@@ -1,5 +1,6 @@
 aws.env <- new.env()
 aws.env$bin <- "aws"
+aws.env$system.prefix <- ""
 
 set_aws <- function(bin) {
   aws.env$bin <- bin
@@ -13,7 +14,12 @@ get_or_empty <- function(str, prefix="") {
   ifelse(length(str) > 0, sprintf("%s %s", prefix, paste(str, collapse = " ")), "")
 }
 
+set_system_prefix <- function(prefix) {
+  aws.env$system.prefix <- prefix
+}
+
 system_collapse <- function(cmd) {
+  cmd <- paste(aws.env$system.prefix, cmd)
   tryCatch(
     fromJSON(paste(system(cmd, intern=TRUE), collapse="\n")),
     warning = function(w) stop(conditionMessage(w))
